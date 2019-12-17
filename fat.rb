768bc080234fae7e0930a3a0c7a585d3afa1ab32
Jan Schäfer <jan.schaefer@henkel.com>
2019-12-17 14:09:58 +0100

    fix:syntax2

--- fat.rb
+++ fat.rb
@@ -2,7 +2,7 @@ class Fat < Formula
     desc "full text search with preview"
     homepage "https://github.com/jancschaefer/fat"
     
-    url "https://github.com/jancschaefer/fat/archive/master.zip", :using =>: curl
+    url "https://github.com/jancschaefer/fat/archive/master.zip", :using => :curl
     
     def install
     bin.install "jancschaefer/fat"
