8c691474b6e316b901a7fe4e57c305eaa2c8ca25
Jan Schäfer <jan.schaefer@henkel.com>
2019-12-17 14:04:24 +0100

    added fat formula

---
+++ fat.rb
@@ -0,0 +1,10 @@
+class Fat < Formula
+    desc "full text search with preview"
+    homepage "https://github.com/jancschaefer/fat"
+    
+    url "https://github.com/jancschaefer/fat/archive/master.zip",: using =>: curl
+    
+    def install
+    bin.install "jancschaefer/fat"
+    end
+    end
