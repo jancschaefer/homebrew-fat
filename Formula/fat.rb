39948eea3a46562e459e5eeb6d6ecaeac103dc73
Jan Schäfer <jan.schaefer@henkel.com>
2019-12-17 14:40:21 +0100

    fix: install

--- Formula/fat.rb
+++ Formula/fat.rb
@@ -1,10 +1,11 @@
 class Fat < Formula
-    desc "full text search with preview"
+    desc ""
     homepage "https://github.com/jancschaefer/fat"
-    
-    url "https://github.com/jancschaefer/fat/archive/master.zip", :using => :curl
-    
+    url "https://github.com/jancschaefer/fat/archive/0.0.2.zip"
+    head "https://github.com/jancschaefer/fat.git"
+    sha256 "d882092b14c80a1285cce59dd2ee0412cbc0905dff9b3b49c6d96dc3759ea1e3"
+
     def install
-        prefix.install "fat.sh"
+        prefix.install "fat"
     end
 end
