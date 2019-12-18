9cc01775bcb159e0b803ac130bdcab3db75cc9cc
Jan Schäfer <jan.schaefer@henkel.com>
2019-12-18 14:44:30 +0100

    updated to 0.0.7

--- Formula/fat.rb
+++ Formula/fat.rb
@@ -1,8 +1,8 @@
 class Fat < Formula
     desc "Commandline utility that searches full text in the current path and offers a preview."
     homepage "https://github.com/jancschaefer/fat"
-    url "https://github.com/jancschaefer/fat/archive/0.0.6.zip"
-    sha256 "5c8078b7edcb77421841b2441be54619655e3a1c1f8f12c5ff03b510de1505c5"
+    url "https://github.com/jancschaefer/fat/archive/0.0.7.zip"
+    sha256 "bd07ee2a3229015576c113795148f4e4fb013ed2bc0410e0c81602890b211761"
     
     depends_on "bat"
     depends_on "fzf"
